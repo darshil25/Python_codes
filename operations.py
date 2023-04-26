cbf2d3fe6aac8cf21a666ce0f4b90c49e9340d26
Darshil Thummar <thummardarshil9687@gmail.com>
2023-04-26 18:10:44 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,13 @@
+import mathfunction
+
+#Adition#
+print("The sum is:",mathfunction.addition(5,7))
+
+#Subtraction#
+print("The difference is: ",mathfunction.subtraction(1897,99))
+
+#Multiplication#
+print("The multiplication is: ",mathfunction.multiplication(8,6))
+
+#Division#
+print("The division is:",mathfunction.division(625,5))
