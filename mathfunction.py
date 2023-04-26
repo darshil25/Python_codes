5b238ff1559851472b8bd89f4b70ecf4570786aa
Darshil Thummar <thummardarshil9687@gmail.com>
2023-04-26 18:10:52 +0530

    Add files via upload

---
+++ mathfunction.py
@@ -0,0 +1,19 @@
+#Addition#
+def addition(num1,num2):
+
+    return num1+num2
+
+#Subtraction#
+def subtraction(num1,num2):
+
+    return num1-num2
+
+#Multiplication#
+def multiplication(num1,num2):
+
+    return num1*num2
+
+#Division#
+def division(num1,num2):
+
+    return num1/num2
